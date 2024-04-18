4b6a65f73de01e0bd8070a40f180cc6fa536f257
zzang.byun <zzang.byun@partner.samsung.com>
2024-04-18 17:45:41 +0900

    init develop branch

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('테스트 페이지');
 });
 
 module.exports = router;
